b9435afefb08cd9807f1e164e7cfd5029f524b90
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-08-24 21:08:20 +0700

    source code template dashboard zenix

--- site/templates/template/other/otherToString.jsx
+++
@@ -1,9 +0,0 @@
-import index from '!raw-loader!../../../../index.text';
-import documentation from '!raw-loader!../../../../documentation.md';
-import point from '!raw-loader!./Point.jsx';
-
-export default {
-  index,
-  documentation,
-  point: point.replace('../static/point.less', './less/point.less'),
-};
